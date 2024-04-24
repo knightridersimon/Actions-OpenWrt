64c4091faf255fa5e97d9ce1e145c588601a509f
knightridersimon <61079727+knightridersimon@users.noreply.github.com>
2024-04-24 10:22:29 +0800

    Update diy-part1.sh

--- X-wrt/Redmi-AX6000/diy-part1.sh
+++ X-wrt/Redmi-AX6000/diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
-sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
+#echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
+#sed -i '$a src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
